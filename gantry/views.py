49c1eaf48397f073bea56b7422e0638cd663af1f
caetano melone <caetanomelone@gmail.com>
2024-01-25 17:12:24 -0800

    lessen fatality of not receiving the right hook

--- gantry/views.py
+++ gantry/views.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import logging
 import os
 
 from aiohttp import web
@@ -20,7 +21,9 @@ async def collect_job(request: web.Request) -> web.Response:
         return web.Response(status=401, text="invalid token")
 
     if request.headers.get("X-Gitlab-Event") != "Job Hook":
-        return web.Response(status=400, text="invalid event type")
+        logging.error(f"invalid event type {request.headers.get('X-Gitlab-Event')} received from Gitlab.")
+        # return 200 so gitlab doesn't disable the webhook -- this is not fatal
+        return web.Response(status=200)
 
     # will return immediately, but will not block the event loop
     # allowing fetch_job to run in the background
