56157abb71bceda23aa804354a96c18882004d81
caetano melone <caetanomelone@gmail.com>
2024-02-05 12:05:55 -0800

    fix flake8

--- gantry/views.py
+++ gantry/views.py
@@ -21,9 +21,7 @@ async def collect_job(request: web.Request) -> web.Response:
         return web.Response(status=401, text="invalid token")
 
     if request.headers.get("X-Gitlab-Event") != "Job Hook":
-        logging.error(
-            f"invalid event type {request.headers.get('X-Gitlab-Event')} received from Gitlab."
-        )
+        logging.error(f"invalid event type {request.headers.get('X-Gitlab-Event')}")
         # return 200 so gitlab doesn't disable the webhook -- this is not fatal
         return web.Response(status=200)
 
