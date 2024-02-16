88c8f80b54e8cb076ff3bdaa5776b350764e6895
caetano melone <caetanomelone@gmail.com>
2024-02-15 16:16:17 -0800

    formatting

--- gantry/util/k8s.py
+++ gantry/util/k8s.py
@@ -2,6 +2,7 @@ BYTES_TO_MEGABYTES = 1 / 1_000_000
 
 # these functions convert the predictions to k8s friendly format
 
+
 def convert_bytes(bytes: float) -> str:
     """bytes to megabytes"""
     return str(int(bytes * BYTES_TO_MEGABYTES)) + "M"
