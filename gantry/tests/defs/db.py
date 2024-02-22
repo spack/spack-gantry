d2173190c132bf47b5defdf20970e04112a6106b
caetano melone <caetanomelone@gmail.com>
2024-02-21 20:37:45 -0800

    add tests: db client

---
+++ gantry/tests/defs/db.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+# fmt: off
+
+# valid input into insert_node
+NODE_INSERT_DICT = {"uuid": "ec253b04-b1dc-f08b-acac-e23df83b3602", "hostname": "ip-192-168-86-107.ec2.internal", "cores": 24.0, "mem": 196608000000.0, "arch": "amd64", "os": "linux", "instance_type": "i3en.6xlarge"}
---
+++ gantry/tests/test_db.py
@@ -0,0 +1,30 @@
+from gantry.db.insert import insert_job, insert_node
+from gantry.tests.defs.db import NODE_INSERT_DICT
+
+
+async def test_node_insert_race(db_conn):
+    """
+    Tests the situation where two identical jobs are inserted around the same time.
+    The first insert should insert the node and the second should get the id of the node.
+    We differentiate the two inserts by their id, but are identical otherwise.
+    """
+    # the id of this row is 1
+    with open("gantry/tests/sql/insert_node.sql") as f:
+        await db_conn.executescript(f.read())
+
+    # the id of NODE_INSERT_DICT is 2, but the output should be 1
+    assert await insert_node(db_conn, NODE_INSERT_DICT) == 2
+
+
+async def test_insert_node_incomplete(db_conn):
+    """
+    Tests that when missing data is passed to the insert_node function, it returns None.
+    Issues around using lastrowid to get the id of the inserted row were returning
+    old inserted rows, so this ensures that the function returns None when it should.
+    """
+    assert await insert_node(db_conn, {"uuid": None}) is None
+
+
+async def test_insert_job_incomplete(db_conn):
+    """See test_insert_node_incomplete"""
+    assert await insert_job(db_conn, {"node": None}) is None
