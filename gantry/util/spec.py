1f251606e9a0f9c38d059675048c2c0d83396352
caetano melone <caetanomelone@gmail.com>
2024-03-07 12:27:44 -0800

    allocation API now accepts spec string rather than JSON payload

--- gantry/util/spec.py
+++ gantry/util/spec.py
@@ -1,3 +1,6 @@
+import re
+
+
 def spec_variants(spec: str) -> dict:
     """Given a spec's concrete variants, return a dict in name: value format."""
     # example: +adios2~advanced_debug patches=02253c7,acb3805,b724e6a use_vtkm=on
@@ -27,3 +30,46 @@ def spec_variants(spec: str) -> dict:
                 variants[part[1:]] = False
 
     return variants
+
+
+def parse_alloc_spec(spec: str) -> dict:
+    """
+    Parses a spec in the format emacs@29.2 +json+native+treesitter%gcc@12.3.0
+    and returns a dictionary with the following keys:
+    - pkg_name: str
+    - pkg_version: str
+    - pkg_variants: dict
+    - compiler: str
+    - compiler_version: str
+
+    Returns an empty dict if the spec is invalid.
+
+    This format is specifically used for the allocation API and is documented
+    for the client.
+    """
+
+    # example: emacs@29.2 +json+native+treesitter%gcc@12.3.0
+    spec_pattern = re.compile(r"(.+?)@([\d.]+)\s+(.+?)%([\w-]+)@([\d.]+)")
+
+    match = spec_pattern.match(spec)
+    if not match:
+        return {}
+
+    # groups in order
+    # create a dictionary with the keys and values
+    pkg_name, pkg_version, pkg_variants, compiler_name, compiler_version = (
+        match.groups()
+    )
+
+    spec_dict = {
+        "pkg_name": pkg_name,
+        "pkg_version": pkg_version,
+        "pkg_variants": spec_variants(pkg_variants),
+        "compiler_name": compiler_name,
+        "compiler_version": compiler_version,
+    }
+
+    if not spec_dict["pkg_variants"]:
+        return {}
+
+    return spec_dict
