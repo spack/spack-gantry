e01633ad5d4b91df6a35c876fb66d4079926d649
caetano melone <caetanomelone@gmail.com>
2024-08-20 14:42:23 -0300

    Add DB migrations functionality
    Gantry will inevitably deviate from our initial db schema, so we the ability to apply migrations.
    
    Changes to the schema should be added to the `migrations/` folder and added to `gantry/__main__.py` as documented in [docs/deploy.md](docs/deploy.md)

--- gantry/tests/conftest.py
+++ gantry/tests/conftest.py
@@ -3,6 +3,8 @@
 import aiosqlite
 import pytest
 
+from gantry.__main__ import apply_migrations
+
 
 @pytest.fixture
 async def db_conn():
@@ -10,7 +12,7 @@ async def db_conn():
     In-memory sqlite connection ensures that the database is clean for each test
     """
     db = await aiosqlite.connect(":memory:")
-    with open("db/schema.sql") as f:
-        await db.executescript(f.read())
+    # apply the schema
+    await apply_migrations(db)
     yield db
     await db.close()
