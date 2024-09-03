8d5ef3bfe631121b04d5d0683155467f5d226bea
caetano melone <caetanomelone@gmail.com>
2024-09-03 20:10:16 -0300

    migration number -> 2

--- gantry/__main__.py
+++ gantry/__main__.py
@@ -27,7 +27,7 @@ async def apply_migrations(db: aiosqlite.Connection):
         # they are applied in the correct order
         # and not inadvertently added to the migrations folder
         ("001_initial.sql", 1),
-        ("003_spec_index.sql", 3),
+        ("002_spec_index.sql", 2),
     ]
 
     # apply migrations that have not been applied
