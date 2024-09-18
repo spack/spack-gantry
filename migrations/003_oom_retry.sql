a79ce6761e1b779752216431d04feb742e61a286
caetano melone <caetanomelone@gmail.com>
2024-09-18 19:03:05 -0300

    clean comment

--- migrations/003_oom_retry.sql
+++ migrations/003_oom_retry.sql
@@ -1,3 +1,2 @@
---- add columns oomed and retry_count to the jobs table default them to 0
 ALTER TABLE jobs ADD COLUMN oomed BOOLEAN DEFAULT FALSE;
 ALTER TABLE jobs ADD COLUMN retry_count INTEGER DEFAULT 0;
