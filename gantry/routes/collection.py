6f978ad594d6197793000c8f14ed9295da077f57
caetano melone <caetanomelone@gmail.com>
2024-09-18 18:49:52 -0300

    add handle_pipeline function to collect webhook - if pipeline failed, check if any of the jobs failed due to OOM - insert OOM jobs into database for prediction step

--- gantry/routes/collection.py
+++ gantry/routes/collection.py
@@ -15,23 +15,79 @@ BUILD_STAGE_REGEX = r"^stage-\d+$"
 logger = logging.getLogger(__name__)
 
 
+async def handle_pipeline(
+    payload: dict,
+    db_conn: aiosqlite.Connection,
+    gitlab: GitlabClient,
+    prometheus: PrometheusClient,
+) -> None | bool:
+    """
+    Sends any failed jobs from a pipeline to fetch_job.
+    If any of the failed jobs were OOM killed, the pipeline will be recreated.
+
+    args:
+        payload: a dictionary containing the information from the Gitlab pipeline hook
+        db: an active aiosqlite connection
+        gitlab: gitlab client
+        prometheus: prometheus client
+
+    returns: True if the pipeline was recreated, else None
+    """
+
+    if payload["object_attributes"]["status"] != "failed":
+        return
+
+    ref = payload["object_attributes"]["ref"]
+    failed_jobs = [
+        # imitate the payload from the job hook, which fetch_jobs expects
+        {
+            "build_status": job["status"],
+            "build_id": job["id"],
+            "build_started_at": job["started_at"],
+            "build_finished_at": job["finished_at"],
+            "ref": ref,
+            "build_stage": job["stage"],
+            "runner": job["runner"],
+        }
+        for job in payload["builds"]
+        if job["status"] == "failed"
+    ]
+
+    retry_pipeline = False
+
+    for job in failed_jobs:
+        # insert every potentially oomed job
+        oomed = await fetch_job(job, db_conn, gitlab, prometheus)
+        # fetch_job can return None or (job_id: int, oomed: bool)
+        if oomed and oomed[1]:
+            retry_pipeline = True
+
+    # once all jobs are collected/discarded, retry the pipeline if needed
+    if retry_pipeline:
+        await gitlab.start_pipeline(ref)
+        print("hi")
+        return True
+
+
 async def fetch_job(
     payload: dict,
     db_conn: aiosqlite.Connection,
     gitlab: GitlabClient,
     prometheus: PrometheusClient,
-) -> None:
+) -> tuple[int, bool] | None:
     """
+    Collects a job's information from Prometheus and inserts into db.
+    Warnings about missing data will be logged; check uncaught exceptions.
     Fetches a job's information from Prometheus and inserts it into the database.
-    If there is data missing at any point, the function will still return so the webhook
-    responds as expected. If an exception is thrown, that behavior was unanticipated by
-    this program and should be investigated.
 
     args:
-        payload: a dictionary containing the information from the Gitlab job hook
+        payload: a dictionary containing the information from the gitlab job hook
         db: an active aiosqlite connection
+        gitlab: gitlab client
+        prometheus: prometheus client
 
-    returns: None in order to accommodate a 200 response for the webhook.
+    returns: if data was inserted,
+                a tuple of the job id and if the job was OOM killed, else None
     """
 
     job = Job(
@@ -63,14 +119,18 @@ async def fetch_job(
         logger.warning(f"job {job.gl_id} is a ghost, skipping")
         return
 
+    # track if job was OOM killed and needs to be retried
+    oomed = False
+
     try:
         annotations = await prometheus.job.get_annotations(job.gl_id, job.midpoint)
+        # check if failed job was OOM killed,
+        # return early if it wasn't because we don't care about it anymore
         if job.status == "failed":
-            # todo does this flow need to exist here?
-            if await prometheus.job.is_oom():
-                pass
+            if await prometheus.job.is_oom(annotations["pod"], job.start, job.end):
+                oomed = True
             else:
-                pass
+                return
 
         resources, node_hostname = await prometheus.job.get_resources(
             annotations["pod"], job.midpoint
@@ -91,6 +151,7 @@ async def fetch_job(
             "gitlab_id": job.gl_id,
             "job_status": job.status,
             "ref": job.ref,
+            "oomed": oomed,
             **annotations,
             **resources,
             **usage,
@@ -100,8 +161,7 @@ async def fetch_job(
     # job and node will get saved at the same time to make sure
     # we don't accidentally commit a node without a job
     await db_conn.commit()
-
-    return job_id
+    return (job_id, oomed)
 
 
 async def fetch_node(
