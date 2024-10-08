28262ff285b001f859690d7e2b0392390ce38cb0
caetano melone <caetanomelone@gmail.com>
2024-10-08 14:45:41 -0300

    update simulation script to output mem/cpu limit measurements

--- dev/simulate_predict.py
+++ dev/simulate_predict.py
@@ -3,22 +3,38 @@
 # 2. copy the file to a different path and set DB_FILE in a different terminal session
 
 # choose how many jobs to predict and set JOBS_TO_PREDICT
-# in db #2, run `delete from jobs order by id desc limit JOBS_TO_PREDICT;`
+# in the training db, run `delete from jobs order by id desc limit JOBS_TO_PREDICT;`
 # to ensure that we aren't predicting from our training data
 # then run the gantry web server `python -m gantry`
 
 # run this script to simulate the prediction of the last JOBS_TO_PREDICT jobs
 # the script will print the average of the memory and cpu ratios of the predictions
 
+# litestream restore -o db s3://spack-gantry/db
+# cp db test.db
+# cp db train.db
+
+
+# in a console session
+# sqlite3 train.db
+# delete from jobs order by id desc limit 8000;
+# export DB_FILE=data/train.db
+# python -m gantry
+
+# in a different console session
+# export DB_FILE=data/test.db
+# python dev/simulate_predict.py
+
 import asyncio
 import json
 import os
+import time
 from urllib.parse import quote
 
 import aiohttp
 import aiosqlite
 
-JOBS_TO_PREDICT = 4000
+JOBS_TO_PREDICT = 8000
 GANTRY_URL = "http://localhost:8080"
 
 
@@ -99,6 +115,7 @@ async def predict(job, session):
         f"@{j['compiler_version']}"
     )
 
+    start_time = time.monotonic()
     async with session.get(f"{GANTRY_URL}/v1/allocation?spec={quote(spec)}") as resp:
         re = await resp.text()
         try:
@@ -106,32 +123,60 @@ async def predict(job, session):
         except Exception:
             print(f"error: {re} for spec {spec}")
             exit()
+        end_time = time.monotonic()
 
-        mem_prediction = re["variables"]["KUBERNETES_MEMORY_REQUEST"]
         # remove M from the end i.e 200M -> 200
-        mem_prediction = int(mem_prediction[:-1])
-        cpu_prediction = float(re["variables"]["KUBERNETES_CPU_REQUEST"])
+        pred_mem_mean = int(re["variables"]["KUBERNETES_MEMORY_REQUEST"][:-1])
+        pred_cpu_mean = (
+            float(re["variables"]["KUBERNETES_CPU_REQUEST"][:-1]) / 1000
+        )  # millicores
+        pred_mem_max = int(re["variables"]["KUBERNETES_MEMORY_LIMIT"][:-1])
+        pred_cpu_max = float(re["variables"]["KUBERNETES_CPU_LIMIT"][:-1]) / 1000
+        mem_mean = j["mem_mean"] / 1_000_000  # bytes to MB
+        cpu_mean = j["cpu_mean"]
+        mem_max = j["mem_max"] / 1_000_000
+        cpu_max = j["cpu_max"]
 
-        mem_usage = j["mem_mean"] / 1_000_000  # bytes to MB
+        ratios = {
+            "mem_mean": (mem_mean / pred_mem_mean),
+            "cpu_mean": (cpu_mean / pred_cpu_mean),
+            "mem_max": (mem_max / pred_mem_max),
+            "cpu_max": (cpu_max / pred_cpu_max),
+            "time": round((end_time - start_time) * 1000, 2),
+        }
 
-        mem_ratio = (mem_usage) / mem_prediction
-        cpu_ratio = j["cpu_mean"] / cpu_prediction
-
-        return mem_ratio, cpu_ratio
+        return ratios
 
 
 async def main():
     jobs = await get_jobs()
-    mem_preds = []
-    cpu_preds = []
+    ratios = {"mem_mean": [], "cpu_mean": [], "mem_max": [], "cpu_max": [], "time": []}
+
     async with aiohttp.ClientSession() as session:
         for job in jobs:
             pred = await predict(job, session)
-            mem_preds.append(pred[0])
-            cpu_preds.append(pred[1])
+            ratios["mem_mean"].append(pred["mem_mean"])
+            ratios["cpu_mean"].append(pred["cpu_mean"])
+            ratios["mem_max"].append(pred["mem_max"])
+            ratios["cpu_max"].append(pred["cpu_max"])
+            ratios["time"].append(pred["time"])
 
-    print(f"average memory ratio: {sum(mem_preds)/len(mem_preds)}")
-    print(f"average cpu ratio: {sum(cpu_preds)/len(cpu_preds)}")
+    def avg(lst):
+        return round(sum(lst) / len(lst), 4)
+
+    print(f"ratio: mean mem usage {avg(ratios['mem_mean'])}")
+    print(f"ratio: mean cpu usage {avg(ratios['cpu_mean'])}")
+    print(f"ratio: max mem usage {avg(ratios['mem_max'])}")
+    print(f"ratio: max cpu usage {avg(ratios['cpu_max'])}")
+    # how many jobs went over the limit
+    print(f"{len([x for x in ratios['mem_max'] if x > 1])} jobs killed")
+
+    if os.environ.get("PROFILE") == "1":
+        print("\n")
+        print(f"mean time per prediction: {round(avg(ratios['time']), 2)}ms")
+        print(f"min time per prediction: {min(ratios['time'])}ms")
+        print(f"max time per prediction: {max(ratios['time'])}ms")
+        print(f"total time: {round(sum(ratios['time'])/1000, 2)}s")
 
 
 if __name__ == "__main__":
