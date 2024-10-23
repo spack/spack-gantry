eaa0f3a7f8f2198681772cf684957d4b78b51b04
Caetano Melone <cmelone@users.noreply.github.com>
2024-10-23 18:36:43 -0300

    Disable checks that set an allocation floor (#119)
    * Disable checks that set an allocation floor
    
    When we were thinking of starting with enabling requests, we wanted to ensure that allocations were never lower than current levels.
    
    However, now that we are enabling limits and requests at the same time, along with more protections if jobs are accidentally killed, we can remove this feature.
    
    * prediction pkg dir -> prediction.py

--- gantry/views.py
+++ gantry/views.py
@@ -6,7 +6,7 @@ import os
 from aiohttp import web
 
 from gantry.routes.collection import fetch_job
-from gantry.routes.prediction.prediction import predict
+from gantry.routes.prediction import predict
 from gantry.util.spec import parse_alloc_spec
 
 logger = logging.getLogger(__name__)
@@ -68,7 +68,4 @@ async def allocation(request: web.Request) -> web.Response:
     if not parsed_spec:
         return web.Response(status=400, text="invalid spec")
 
-    # we want to keep predictions >= current levels (with ensure_higher strategy)
-    return web.json_response(
-        await predict(request.app["db"], parsed_spec, strategy="ensure_higher")
-    )
+    return web.json_response(await predict(request.app["db"], parsed_spec))
